2cee7155b0069ac50aa504e78205145824f86d62
Arun Bharath <arunbharath91@gmail.com>
2021-08-04 23:49:07 +0530

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,74 @@
 const path = require('path');
-const {
-  CleanWebpackPlugin
-} = require('clean-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const WebpackMd5Hash = require("webpack-md5-hash");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: ['./src/app/main'],
   devtool: 'inline-source-map',
   module: {
-    rules: [{
-      test: /\.ts$/,
-      use: 'ts-loader',
-      exclude: [/node_modules\/rxjs/],
-    }, {
-      test: /\.scss$/,
-      use: [
-        "style-loader",
-        MiniCssExtractPlugin.loader,
-        "css-loader",
-        "sass-loader"
-      ]
-    }],
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          // Compiles Sass to CSS
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+              implementation: require("sass"),
+              sassOptions: {
+                fiber: false,
+                outputStyle: "compressed",
+              },
+            },
+          }
+        ],
+      }
+    ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: "style.css"
-    }),
     new HtmlWebpackPlugin({ // Also generate a test.html
       filename: 'index.html',
       template: 'src/index.html',
       hash: true,
     }),
     new CleanWebpackPlugin(),
-    new CopyPlugin([{
-        from: 'src/assets',
-        to: 'assets',
-        ignore: ['*.scss'],
-      },
-      {
-        from: 'src/*.html',
-        to: '',
-        flatten: true,
-        ignore: ['index.html']
-      }
-    ]),
-    new WebpackMd5Hash()
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  }
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src/assets',
+          to: 'assets',
+          noErrorOnMissing: true,
+          globOptions: {
+            ignore: [
+              // Ignore all `txt` files
+              "**/*.scss"
+            ],
+          },
+        },
+
+      ]
+    })
+  ]
 };
